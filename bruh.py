c8d8e2d0d9ebdc32f2c87e668e1d0cf7d98c810c
Peepatza <kpeepat@gmail.com>
2025-10-21 14:02:58 +0700

    v4

---
+++ bruh.py
@@ -0,0 +1,10 @@
+#bruh it aint working
+print("Hello, world")
+print("Hiw kaow mak mak")
+print("how to commit bruh")
+
+
+# ctrl+/ cmd for comment
+
+
+black_monkey("I", "HATE", "monkey", sep="67")
