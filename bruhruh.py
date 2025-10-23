68a34e7d3cf857afda8cf2c96b6e09e3d767507d
Peepatza <kpeepat@gmail.com>
2025-10-23 13:44:40 +0700

    cmon

---
+++ bruhruh.py
@@ -0,0 +1,22 @@
+#bruh it aint working
+print("Hello, world")
+print("Hiw kaow mak mak")
+print("how to commit bruh")
+
+print("I", "HATE", "monkey", sep="67", end=())
+
+#strings
+#"whatever inside this"
+
+#float
+#4.12414124
+
+#int
+#1, 2 ,3 ,4 ,5
+
+# Boolean 
+#True,False
+
+#nonetype the lack of data
+#none
+
